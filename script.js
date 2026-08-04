ec7d09375da91c8af1883174281538c49e43653b
retroreminder00 <jedimasterbj@gmail.com>
2026-08-04 12:52:01 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
-// Retro Reminder TV controls
-// Channel controls will go here.
+const channels = [
+    {
+        number: "03",
+        name: "YouTube",
+        url: "https://youtube.com/@retro.reminder?si=RgH9yUZVI90Dgy-k"
+    },
+    {
+        number: "07",
+        name: "TikTok",
+        url: "https://www.tiktok.com/@retro.reminder"
+    },
+    {
+        number: "11",
+        name: "Instagram",
+        url: "https://www.instagram.com/retro.reminder"
+    },
+    {
+        number: "22",
+        name: "Facebook",
+        url: "https://www.facebook.com/share/1JAfe51abo/"
+    },
+    {
+        number: "99",
+        name: "eBay",
+        url: "https://www.ebay.com/usr/retro.reminder"
+    }
+];
+
+let currentChannel = 0;
