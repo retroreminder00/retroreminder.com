14a2603987d7a2eb017120ae5657fa14986ee4eb
retroreminder00 <jedimasterbj@gmail.com>
2026-08-04 13:05:29 -0600

    Update script.js

--- script.js
+++ script.js
@@ -41,23 +41,51 @@ function changeChannel() {
 
     const screen = document.querySelector(".tv-screen");
 
-    screen.innerHTML = `
-        <h2>CH ${channel.number}</h2>
+    /* TV STATIC */
 
-        <div class="boot-text">
-            NOW PLAYING
-        </div>
-
-        <p class="blink">
-            ${channel.name}
-        </p>
-
-        <p>
-            CLICK THE SCREEN TO VISIT
-        </p>
+    screen.style.background = `
+        repeating-linear-gradient(
+            0deg,
+            #111 0px,
+            #111 2px,
+            #777 3px,
+            #222 4px
+        )
     `;
 
-    screen.onclick = function() {
-        window.open(channel.url, "_blank");
-    };
+    screen.style.color = "white";
+
+    screen.innerHTML = `
+        <h2 style="color:#00d9ff;">
+            ▓▒░ STATIC ░▒▓
+        </h2>
+    `;
+
+    /* CHANGE CHANNEL AFTER STATIC */
+
+    setTimeout(function() {
+
+        screen.style.background = "#050505";
+
+        screen.innerHTML = `
+            <h2>CH ${channel.number}</h2>
+
+            <div class="boot-text">
+                NOW PLAYING
+            </div>
+
+            <p class="blink">
+                ${channel.name}
+            </p>
+
+            <p>
+                CLICK THE SCREEN TO VISIT
+            </p>
+        `;
+
+        screen.onclick = function() {
+            window.open(channel.url, "_blank");
+        };
+
+    }, 250);
 }
